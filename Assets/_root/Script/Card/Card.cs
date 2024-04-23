3e51ef103114c93e98a2067f13f241d67b29cd30
Mooner510 <lmck510@gmail.com>
2024-04-23 17:31:20 +0900

    rename all

---
+++ Assets/_root/Script/Card/Card.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace _root.Script.Card
+{
+    public class Card : MonoBehaviour
+    {
+        [HideInInspector] public SpriteRenderer frontSide;
+        [HideInInspector] public SpriteRenderer backSide;
+
+        public void Start()
+        {
+            frontSide = transform.GetChild(0).GetComponent<SpriteRenderer>();
+            backSide = transform.GetChild(1).GetComponent<SpriteRenderer>();
+            backSide.sprite = Resources.Load<Sprite>("Card/card_frame");
+        }
+    }
+}
---
+++ Assets/_root/Script/Card/Editor/CardEditor.cs
@@ -0,0 +1,22 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace _root.Script.Card.Editor
+{
+    [CustomEditor(typeof(Card))]
+    public class CardEditor : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            var card = (Card)target;
+
+            if (card.frontSide == null) card.frontSide = card.transform.GetChild(0).GetComponent<SpriteRenderer>();
+            if (card.backSide == null) card.backSide = card.transform.GetChild(1).GetComponent<SpriteRenderer>();
+
+            card.frontSide.sprite = (Sprite)EditorGUILayout.ObjectField("Front", card.frontSide.sprite, typeof(Sprite), false);
+            // card.backSide.sprite = (Sprite)EditorGUILayout.ObjectField("Back", card.backSide.sprite, typeof(Sprite), false);
+        }
+    }
+}
