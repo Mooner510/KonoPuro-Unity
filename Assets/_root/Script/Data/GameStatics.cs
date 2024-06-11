b340d9a84b82c742959cf5ba33a2778ad04f4fa3
hsung3407 <azxccxzavv3407@gmail.com>
2024-06-11 20:22:37 +0900

    Update GameStatics.cs

--- Assets/_root/Script/Data/GameStatics.cs
+++ Assets/_root/Script/Data/GameStatics.cs
@@ -10,19 +10,21 @@ public static class GameStatics
 {
 	public static int dDay = 15;
 
-	public static UpdatedData self;
-	public static UpdatedData other;
-	public static bool isTurn;
+	public static int         gatchaOncePrice  = 100;
+	public static int         gatchaMultiPrice = 999;
 
-	public static int gatchaOncePrice = 100;
-	public static int gatchaMultiPrice = 999;
+	public static int         deckCharacterCardRequired = 5;
+	public static int         deckUseCardRequired       = 25;
 
-	public static int deckCharacterCardRequired = 5;
-	public static int deckUseCardRequired = 25;
-	
 	public static List<GatchaResponse> gatchaList;
 
-	public static int maxProgress = 1000;
-	public static float CalcTotalProgress(int sum) => (float)sum / (Enum.GetValues(typeof(MajorType)).Length * maxProgress);
+	public static int         maxProgress = 1000;
+	public static float       CalcTotalProgress(int sum) => (float)sum / (Enum.GetValues(typeof(MajorType)).Length * maxProgress);
+
+	//동적 값들
+	public static UpdatedData self;
+	public static UpdatedData other;
+	public static bool        isTurn;
+	public static GameCard    useCard;
 }
 }
