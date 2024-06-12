ce78d87043f0c1ddbc67ac75a2e2a0079d0114e0
hsung3407 <azxccxzavv3407@gmail.com>
2024-06-12 16:10:08 +0900

    update: title

--- Assets/_root/Script/Title/FirstCard.cs
+++ Assets/_root/Script/Title/FirstCard.cs
@@ -3,27 +3,25 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 public class FirstCard : MonoBehaviour {
     private Camera camera; 
-    private GameObject todayscard;
+    private GameObject todayCard;
+    
     private void Awake()
     {
-        todayscard = GameObject.Find("Todayscard");
+        todayCard = GameObject.Find("Today's Card");
     }
 
     private void Start()
     {
-        todayscard.SetActive(false);
+        todayCard.SetActive(false);
         camera = Camera.main;
     }
 
     void Update(){
-        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
-        if (Input.GetMouseButtonDown(0))
+        var ray = camera.ScreenPointToRay(Input.mousePosition);
+        if (!Input.GetMouseButtonDown(0) || EventSystem.current.IsPointerOverGameObject()) return;
+        if (Physics.Raycast(ray, out var hit, 20, 1 << 6))
         {
-            if (EventSystem.current.IsPointerOverGameObject()) return;
-            if (Physics.Raycast(ray, out var hit, 20, 1 << 6))
-            {
-                todayscard.SetActive(true);
-            }
+            todayCard.SetActive(true);
         }
     }
 }
