fef1ab55eb4309257be02836516304fecb9d2caf
hsung3407 <azxccxzavv3407@gmail.com>
2024-06-20 22:56:51 +0900

    fix: other ability

--- Assets/_root/Script/Main/Deck/DeckCardFilterMenu.cs
+++ Assets/_root/Script/Main/Deck/DeckCardFilterMenu.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using _root.Script.Network;
 using TMPro;
 using UnityEngine;
@@ -5,64 +7,85 @@ using UnityEngine.UI;
 
 public class DeckCardFilterMenu : MonoBehaviour
 {
-    [SerializeField]
-    private GameObject majorTogglePrefab;
+	private GameObject studentFilter;
+	private GameObject useCardFilter;
 
-    private GameObject studentFilter;
-    private GameObject useCardFilter;
+	private Toggle[] studentMajors;
+	private Toggle[] studentTiers;
 
-    private Toggle[] studentMajors;
-    private Toggle[] studentTiers;
+	private Toggle[] useCardTiers;
 
-    private Toggle[] useCardTiers;
+	public List<MajorType> SelectedMajors()
+	{
+		List<MajorType> selected = new List<MajorType>();
+		for (var i = 0; i < studentMajors.Length; i++)
+			if (studentMajors[i].isOn) selected.Add((MajorType)i);
+		return selected;
+	}
 
-    // Start is called before the first frame update
-    private void Awake()
-    {
-        studentFilter = transform.GetChild(0).gameObject;
-        useCardFilter = transform.GetChild(1).gameObject;
-        
-        studentMajors = studentFilter.transform.GetChild(0).GetComponentsInChildren<Toggle>();
-        for (var i = 0; i < studentMajors.Length; i++)
-        {
-            studentMajors[i].transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = ((MajorType)i).ToString();
-        }
+	public List<int> SelectedTier(bool character)
+	{
+		List<int> selected = new List<int>();
+		if(character)
+		{
+			for (var i = 0; i < studentTiers.Length; i++)
+				if (studentTiers[i].isOn) selected.Add(i);
+		}
+		else
+		{
+			for (var i = 0; i < useCardTiers.Length; i++)
+				if(useCardTiers[i].isOn) selected.Add(i);
+		}
+		return selected;
+	}
 
-        studentTiers = studentFilter.transform.GetChild(1).GetComponentsInChildren<Toggle>();
-        for (var i = 3; i > 1; i--)
-        {
-            studentTiers[i].transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = $"티어 {i}";
-        }
-        
-        useCardTiers = useCardFilter.transform.GetChild(0).GetComponentsInChildren<Toggle>();
-        for (var i = 3; i > 1; i--)
-        {
-            studentTiers[i].transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = $"티어 {i}";
-        }
-    }
+	// Start is called before the first frame update
+	private void Awake()
+	{
+		studentFilter = transform.GetChild(0).gameObject;
+		useCardFilter = transform.GetChild(1).gameObject;
 
-    private void Start()
-    {
-        SetType(true);
-        Show(false);
-    }
+		studentMajors = studentFilter.transform.GetChild(0).GetComponentsInChildren<Toggle>();
+		for (var i = 0; i < studentMajors.Length; i++)
+		{
+			studentMajors[i].GetComponentInChildren<TextMeshProUGUI>().text = ((MajorType)i).ToString();
+		}
 
-    private void Init()
-    {
-        foreach (var toggle in studentMajors) toggle.isOn = true;
-        foreach (var toggle in studentTiers) toggle.isOn = true;
-        foreach (var toggle in useCardTiers) toggle.isOn = true;
-    }
+		studentTiers = studentFilter.transform.GetChild(1).GetComponentsInChildren<Toggle>();
+		for (var i = 3; i > -1; i--)
+		{
+			studentTiers[i].transform.GetComponentInChildren<TextMeshProUGUI>().text = $"티어 {i}";
+		}
 
-    public void SetType(bool character)
-    {
-        Init();
-        studentFilter.SetActive(character);
-        useCardFilter.SetActive(!character);
-    }
+		useCardTiers = useCardFilter.transform.GetChild(0).GetComponentsInChildren<Toggle>();
+		for (var i = 3; i > -1; i--)
+		{
+			useCardTiers[i].GetComponentInChildren<TextMeshProUGUI>().text = $"티어 {i}";
+		}
+	}
 
-    public void Show(bool show)
-    {
-        gameObject.SetActive(show);
-    }
+	private void Start()
+	{
+		SetType(true);
+		Show(false);
+	}
+
+	public void Init()
+	{
+		foreach (var toggle in studentMajors) toggle.isOn = true;
+		foreach (var toggle in studentTiers) toggle.isOn  = true;
+		foreach (var toggle in useCardTiers) toggle.isOn  = true;
+	}
+
+	public void SetType(bool character)
+	{
+		Init();
+		studentFilter.SetActive(character);
+		useCardFilter.SetActive(!character);
+	}
+
+	public void Show(bool show)
+	{
+		gameObject.SetActive(show);
+	}
 }
