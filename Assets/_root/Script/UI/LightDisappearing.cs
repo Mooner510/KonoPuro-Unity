5381cd2485e5d4efafacc43e97513f6d3cce4e22
minding2796 <minding2796@gmail.com>
2024-06-21 12:33:48 +0900

    Add AudioFiles

--- Assets/_root/Script/UI/LightDisappearing.cs
+++ Assets/_root/Script/UI/LightDisappearing.cs
@@ -1,9 +1,16 @@
+using System;
+using _root.Script.Manager;
 using UnityEngine;
 
 namespace _root.Script.UI
 {
     public class LightDisappearing : MonoBehaviour
     {
+        private void Start()
+        {
+            AudioManager.PlaySoundInstance("Audio/CARD_TIER_UNPACK");
+        }
+
         private void Update()
         {
             GetComponent<Light>().intensity = Mathf.Lerp(GetComponent<Light>().intensity, 0, 0.1f);
